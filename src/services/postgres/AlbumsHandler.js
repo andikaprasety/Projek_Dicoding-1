9b7d5658ebe1a2e6c1eeff3bb08610ec439ecedc
gemm123 <gemaakbar213@gmail.com>
2022-12-27 17:44:42 +0700

    feat: addAlbum handler

---
+++ src/services/postgres/AlbumsHandler.js
@@ -0,0 +1,30 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class AlbumsService {
+    constructor() {
+        this._pool = new Pool();
+    }
+
+    async addAlbum({ name, year }) {
+        const id = nanoid(16);
+        const createdAt = new Date().toISOString();
+        const updatedAt = createdAt;
+
+        const query = {
+            text: 'INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id',
+            values: [id, name, year, createdAt, updatedAt],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows[0].id) {
+            throw new InvariantError('Album gagal ditambahkan');
+        }
+
+        return result.rows[0].id;
+    }
+}
+
+module.exports = AlbumsService;
