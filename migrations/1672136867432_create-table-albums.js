0fcb97073c9aa3e11dece43e3d03e622f54ab608
gemm123 <gemaakbar213@gmail.com>
2022-12-27 17:37:00 +0700

    feat: migrate albums

---
+++ migrations/1672136867432_create-table-albums.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+    pgm.createTable('albums', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        name: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        year: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('albums');
+};
