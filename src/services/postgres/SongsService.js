5d85bb834c5e0e90b733cde0726cbb2bcd0e5cdd
gemm123 <gemaakbar213@gmail.com>
2022-12-28 19:35:03 +0700

    feat: delete song by id

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -74,6 +74,19 @@ class SongsService {
             throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
         }
     }
+
+    async deleteSongById(id) {
+        const query = {
+            text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+            values: [id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan');
+        }
+    }
 }
 
 module.exports = SongsService;
