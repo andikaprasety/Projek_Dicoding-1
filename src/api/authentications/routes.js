c8a4deab64fca45340e3840fb6a9d2a1a3171abf
gemm123 <gemaakbar213@gmail.com>
2023-01-01 19:12:59 +0700

    fix: routes postAuthentications

--- src/api/authentications/routes.js
+++ src/api/authentications/routes.js
@@ -2,7 +2,7 @@ const routes = (handler) => [
     {
       method: 'POST',
       path: '/authentications',
-      handler: handler.postAuthenticationHandler,
+      handler: handler.postAuthenticationsHandler,
     },
   ];
 
